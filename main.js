5db758eafe3bffcb752b3ece81a15e62bb9cc0c5
runweiting <whatisfuntime@gmail.com>
2023-11-04 12:56:02 +0800

    feat: 修正卡片套票 html 結構

--- main.js
+++ main.js
@@ -32,58 +32,95 @@ let data = [
       "rate": 7
     }
 ];
+
+// 共用 DOM
+let ticketCard = document.querySelector('.ticketCard');
+
 // 初始化網頁畫面
 function init(){
-    let ticketCard = document.querySelector('.ticketCard');
     let str = '';
     data.forEach(function(item){
-        str += `<li class="col card border-0 shadow position-relative px-0">
-        <span class="tag-l position-absolute top-0 start-0 translate-middle-y bg-secondary fs-5 fw-normal text-white">${item.area}</span>
-        <img src="${item.imgUrl}" class="card-img-top mb-1" alt="travel_1" style="height: 180px;">
-        <span class="tag-m position-absolute bg-primary fw-normal text-white">${item.rate}</span>
-        <div class="d-flex flex-column p-4" style="height: 296px;">
-        <h3 class="card-title fs-4 fw-medium text-primary pb-1 border-bottom border-2 border-primary">${item.name}</h3>
-        <p class="fw-normal text-info flex-grow-1">${item.description}</p>
-        <div class="d-flex justify-content-between">
-            <div class="d-flex align-items-center">
-            <span class="material-symbols-outlined fs-5 text-primary me-1">
-                info
-            </span>
-            <p class="fw-medium text-primary mb-0">剩下最後 ${item.group} 組</p>
+        let content = `<li class="col-4">
+        <div class="card border-0 shadow position-relative">
+          <span class="tag-l position-absolute top-0 start-0 translate-middle-y bg-secondary fs-5 fw-normal text-white">${item.area}</span>
+          <img src="${item.imgUrl}" class="card-img-top mb-1" alt="travel_1" style="height: 180px;">
+          <span class="tag-m position-absolute bg-primary fw-normal text-white">${item.rate}</span>
+          <div class="d-flex flex-column p-4" style="height: 296px;">
+            <h3 class="card-title fs-4 fw-medium text-primary pb-1 border-bottom border-2 border-primary">${item.name}</h3>
+            <p class="fw-normal text-info flex-grow-1">${item.description}</p>
+            <div class="d-flex justify-content-between">
+              <div class="d-flex align-items-center">
+                <span class="material-symbols-outlined fs-5 text-primary me-1">
+                  info
+                </span>
+                <p class="fw-medium text-primary mb-0">剩下最後 ${item.group} 組</p>
+              </div>
+              <div class="d-flex align-items-center">
+                <span class="fw-medium text-primary me-1">TWD</span>
+                <p class="display-8 fw-medium text-primary mb-0">${item.price}</p>
+              </div>
             </div>
-            <div class="d-flex align-items-center">
-            <span class="fw-medium text-primary me-1">TWD</span>
-            <p class="display-8 fw-medium text-primary mb-0">${item.price}</p>
-            </div>
+          </div>
         </div>
-        </div>
-    </li>`
+      </li>`;
+        str += content
     });
     ticketCard.innerHTML = str;
 }
 init();
 
-// 地區搜尋
-let regionSearch = document.querySelector('#regionSearch');
+// select 地區搜尋 + 監聽
+const regionSearch = document.querySelector('#regionSearch');
+
 regionSearch.addEventListener('change',function(e){
-    if ( e.target.value == undefined ){
-        return
-    }
-    // 跑 forEach，做判斷，組字串，顯示畫面上
+    // 預備 卡片套票 的集合
     let str = '';
-    data.forEach(function(item){
+
+    // 預備 搜尋資料 的集合
+    let dataNum = [];
+
+    // select 地區搜尋
+    data.forEach(function(item,index){
+        // select 全部地區
+        if ( e.target.value == "全部地區" ){
+            let content = `<li class="col-4">
+            <div class="card border-0 shadow position-relative">
+              <span class="tag-l position-absolute top-0 start-0 translate-middle-y bg-secondary fs-5 fw-normal text-white">${item.area}</span>
+              <img src="${item.imgUrl}" class="card-img-top mb-1" alt="travel_1" style="height: 180px;">
+              <span class="tag-m position-absolute bg-primary fw-normal text-white">${item.rate}</span>
+              <div class="d-flex flex-column p-4" style="height: 296px;">
+                <h3 class="card-title fs-4 fw-medium text-primary pb-1 border-bottom border-2 border-primary">${item.name}</h3>
+                <p class="fw-normal text-info flex-grow-1">${item.description}</p>
+                <div class="d-flex justify-content-between">
+                  <div class="d-flex align-items-center">
+                    <span class="material-symbols-outlined fs-5 text-primary me-1">
+                      info
+                    </span>
+                    <p class="fw-medium text-primary mb-0">剩下最後 ${item.group} 組</p>
+                  </div>
+                  <div class="d-flex align-items-center">
+                    <span class="fw-medium text-primary me-1">TWD</span>
+                    <p class="display-8 fw-medium text-primary mb-0">${item.price}</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </li>`;
+            str += content
+        };
+        // select 其他地區
         if ( e.target.value == item.area ){
-            str += `<li class="col-4 card border-0 shadow position-relative px-0">
-        <span class="tag-l position-absolute top-0 start-0 translate-middle-y bg-secondary fs-5 fw-normal text-white">${item.area}</span>
-        <img src="${item.imgUrl}" class="card-img-top mb-1" alt="travel_1" style="height: 180px;">
-        <span class="tag-m position-absolute bg-primary fw-normal text-white">${item.rate}</span>
-        <div class="d-flex flex-column p-4" style="height: 296px;">
-        <h3 class="card-title fs-4 fw-medium text-primary pb-1 border-bottom border-2 border-primary">${item.name}</h3>
-        <p class="fw-normal text-info flex-grow-1">${item.description}</p>
-        <div class="d-flex justify-content-between">
+            let content = `<li class="col-4 card border-0 shadow position-relative px-0">
+            <span class="tag-l position-absolute top-0 start-0 translate-middle-y bg-secondary fs-5 fw-normal text-white">${item.area}</span>
+            <img src="${item.imgUrl}" class="card-img-top mb-1" alt="travel_1" style="height: 180px;">
+            <span class="tag-m position-absolute bg-primary fw-normal text-white">${item.rate}</span>
+            <div class="d-flex flex-column p-4" style="height: 296px;">
+            <h3 class="card-title fs-4 fw-medium text-primary pb-1 border-bottom border-2 border-primary">${item.name}</h3>
+            <p class="fw-normal text-info flex-grow-1">${item.description}</p>
+            <div class="d-flex justify-content-between">
             <div class="d-flex align-items-center">
             <span class="material-symbols-outlined fs-5 text-primary me-1">
-                info
+            info
             </span>
             <p class="fw-medium text-primary mb-0">剩下最後 ${item.group} 組</p>
             </div>
@@ -91,12 +128,21 @@ regionSearch.addEventListener('change',function(e){
             <span class="fw-medium text-primary me-1">TWD</span>
             <p class="display-8 fw-medium text-primary mb-0">${item.price}</p>
             </div>
-        </div>
-        </div>
-    </li>`
-        }
+            </div>
+            </div>
+            </li>`;
+            str += content
+        };
+        dataNum.push(index)
     });
-    let ticketCard = document.querySelector('.ticketCard');
     ticketCard.innerHTML = str;
-})
+
+    // 顯示 搜尋資料 的筆數
+    const searchResult = document.querySelector('.searchResult');
+    searchResult.innerHTML = `<p class="searchResult mb-0 text-info" style="width: 255px">本次搜尋共 ${dataNum.length} 筆資料</p>`;
+});
+
+
+
+
 
