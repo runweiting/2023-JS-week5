803325fd43290ac6b6d49c74e22e84fbbf591df1
runweiting <whatisfuntime@gmail.com>
2023-11-11 09:58:11 +0800

    feat: 透過 axios 撈外部資料

--- main.js
+++ main.js
@@ -1,37 +1,12 @@
 import './assets/scss/all.scss';
 
-let data = [
-    {
-      "id": 0,
-      "name": "肥宅心碎賞櫻3日",
-      "imgUrl": "https://images.unsplash.com/photo-1522383225653-ed111181a951?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1655&q=80",
-      "area": "高雄",
-      "description": "賞櫻花最佳去處。肥宅不得不去的超讚景點！",
-      "group": 87,
-      "price": 1400,
-      "rate": 10
-    },
-    {
-      "id": 1,
-      "name": "貓空纜車雙程票",
-      "imgUrl": "https://images.unsplash.com/photo-1501393152198-34b240415948?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80",
-      "area": "台北",
-      "description": "乘坐以透明強化玻璃為地板的「貓纜之眼」水晶車廂，享受騰雲駕霧遨遊天際之感",
-      "group": 99,
-      "price": 240,
-      "rate": 2
-    },
-    {
-      "id": 2,
-      "name": "台中谷關溫泉會1日",
-      "imgUrl": "https://images.unsplash.com/photo-1535530992830-e25d07cfa780?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1650&q=80",
-      "area": "台中",
-      "description": "全館客房均提供谷關無色無味之優質碳酸原湯，並取用八仙山之山冷泉供蒞臨貴賓沐浴及飲水使用。",
-      "group": 20,
-      "price": 1765,
-      "rate": 7
-    }
-];
+// 透過 axios 撈外部資料
+let data = [];
+axios.get('https://raw.githubusercontent.com/hexschool/js-training/main/travelApi.json')
+.then(function(response){
+  data = response.data['data'];
+  renderData();
+})
 
 // 共用 DOM
 let ticketCard = document.querySelector('.ticketCard');
@@ -47,7 +22,7 @@ function renderData(region = "全部地區"){
   searchResult.textContent = filterData.length?
   `本次搜尋共 ${filterData.length} 筆資料` : "查無此關鍵字資料";
   let content = '';
-  filterData.forEach(function(item,index){
+  filterData.forEach(function(item){
       content += `<li class="col-4 mb-9">
       <div class="card border-0 shadow position-relative">
         <span class="tag-l position-absolute top-0 start-0 translate-middle-y bg-secondary fs-5 fw-normal text-white">${item.area}</span>
